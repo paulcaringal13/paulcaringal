3e231ed11d3b1fb54424824166054ae958f99751
paulcaringal13 <132770741+paulcaringal13@users.noreply.github.com>
2023-05-06 23:50:14 +0800

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,11 @@
+// Sticky navbar
+$(document).ready(function() {
+    $(window).on('scroll',function(){
+        var scroll = $(window).scrollTop();
+        if(scroll>=50){
+            $(".sticky").addClass("stickyadd");
+        }else {
+            $(".sticky").removeClass("stickyadd");
+        }
+    })
+})
